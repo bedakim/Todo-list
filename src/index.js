c0195f6cc920caf328a4998bd4a35715820031cc
Kim Seungha <seungha.me@gmail.com>
2018-10-29 19:26:27 +0900

    코드 리뷰

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const todoFormEl = document.querySelector(".todo-form");
 const todoListEl = document.querySelector(".todo-list");
 
+// 수업에서 알려드린 방식대로 잘 하셨습니다!
+// 개선할 수 있는 부분:
+// 1. input에 할 일 텍스트를 입력하지 않고 엔터를 눌러도 항목이 추가되고 있습니다. 이 문제를 고쳐볼 수 있을 것 같습니다. ('required' 어트리뷰트를 넣는다던지, 아니면 직접 JS로 데이터 검증을 한다던지..)
+// 2. 현재 텍스트와 버튼이 연이어 붙어 있어서, 사용하기 편하지 않은 상태입니다. 할 일 텍스트는 왼쪽에, 버튼들은 오른쪽에 몰려 있게 코드를 바꿔보세요.
+// 한 번 시도해보시고, 다 하셨거나 어려우시다면 또 DM 주세요!
+
 todoFormEl.addEventListener("submit", e => {
   e.preventDefault();
   addTodo(e.target.elements.todo.value);
@@ -10,8 +16,9 @@ todoFormEl.addEventListener("submit", e => {
   // e.target.elements.todo.value = ''
 
   //2. 폼의 리셋 사용하기
-  e.target.reset();
+  e.target.reset(); 
 });
+
 function addTodo(newTodoText) {
   // 클릭 한번이 되어야 할때 수행되어야하는 작업
 
